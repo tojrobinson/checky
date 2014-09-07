aad67194f63dd58f5d340fa65315eb7dc11c9c5a
Tully Robinson <tully@tojr.org>
2014-09-08 02:42:41 +1000

    Add RegExp checks

--- test/basic.js
+++ test/basic.js
@@ -6,25 +6,36 @@ var Book = checky({
    author: {
       type: String,
       optional: true
-   }
+   },
+   ISBN: /^[a-zA-Z0-9]+$/
 });
 
 assert.equal(Book({
-   title: 'The C Programming Language'
+   title: 'The C Programming Language',
+   ISBN: 'abc123'
 }), true);
 
 assert.equal(Book({
    title: 'The C Programming Language',
-   author: 'Kernighan and Ritchie'
+   author: 'Kernighan and Ritchie',
+   ISBN: 'abc123'
 }), true);
 
 assert.equal(Book({
-   author: 'Kernighan and Ritchie'
+   title: 'The C Programming Language',
+   author: 'Kernighan and Ritchie',
+   ISBN: 'abc-123'
+}), false);
+
+assert.equal(Book({
+   author: 'Kernighan and Ritchie',
+   ISBN: 'abc123'
 }), false);
 
 assert.equal(Book({
    title: 'The C Programming Language',
-   author: 42
+   author: 42,
+   ISBN: 'abc123'
 }), false);
 
 console.log('All tests passed. You are awesome!');
